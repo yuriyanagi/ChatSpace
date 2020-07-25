1a4b7ac19d90d8a55813ce525b8620d5ecf0f4cc
yuriyanagi <40306yy@gmail.com>
2020-07-25 15:55:18 +0900

    add automatic-updating

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -1,8 +1,9 @@
 $(function(){
+
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="message-box">
+        `<div class="message-box" data-message-id=${message.id}>
           <div class="message-date">
             <div class="name">
               ${message.user_name}
@@ -21,7 +22,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="message-box">
+      `<div class="message-box" data-message-id=${message.id}>
         <div class="message-date">
           <div class="name">
             ${message.user_name}
@@ -64,4 +65,5 @@ $(function(){
     });
   });
 });
+
 
